094745cf65ecbfb8e7251320b4c02df9eabd897f
Michelle Rutherford <michellewwg@gmail.com>
2015-05-11 21:51:18 -0400

    Completed checkpoint 26-building-blocstagram
    :q

--- Blocstagram/Blocstagram/ImagesTableViewController.h
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Michelle Rutherford on 5/11/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
