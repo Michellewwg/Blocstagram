720861eeab8129a371a877aeff299d16bdfdfda2
Michelle Rutherford <michellewwg@gmail.com>
2015-05-27 16:49:52 -0400

    Assignmet 29

--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -60,7 +60,10 @@ static NSParagraphStyle *paragraphStyle;
         
         NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(_mediaImageView, _usernameAndCaptionLabel, _commentLabel);
         
-        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_mediaImageView]|" options:kNilOptions metrics:nil views:viewDictionary]];
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_mediaImageView(==100)]|"
+                                                                                 options:kNilOptions
+                                                                                 metrics:nil
+                                                                                 views:viewDictionary]];
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_usernameAndCaptionLabel]|"
                                                                                  options:kNilOptions metrics:nil
                                                                                  views:viewDictionary]];
@@ -68,17 +71,17 @@ static NSParagraphStyle *paragraphStyle;
                                                                                  options:kNilOptions
                                                                                  metrics:nil
                                                                                  views:viewDictionary]];
-        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_mediaImageView][_usernameAndCaptionLabel][_commentLabel]"
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_mediaImageView(==100)][_usernameAndCaptionLabel][_commentLabel]"
                                                                                 options:kNilOptions
                                                                                 metrics:nil
                                                                                 views:viewDictionary]];
         self.imageHeightConstraint = [NSLayoutConstraint constraintWithItem:_mediaImageView
-                    attribute:NSLayoutAttributeHeight
-                                       relatedBy:NSLayoutRelationEqual
-                                       toItem:nil
-                                       attribute:NSLayoutAttributeNotAnAttribute
-                                       multiplier:1
-                                       constant:100];
+                                                                  attribute:NSLayoutAttributeHeight
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:nil
+                                                                  attribute:NSLayoutAttributeNotAnAttribute
+                                                                 multiplier:1
+                                                                   constant:100];
     
         
         self.imageHeightConstraint.identifier = @"Image height constraint";
