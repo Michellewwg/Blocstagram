3bffb3066d4151f0217c8e862aa2b8bd3b83370a
Michelle Rutherford <michellewwg@gmail.com>
2015-08-16 13:40:24 -0400

    checkpoint-43-filters complete

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -15,6 +15,7 @@
 #import "mediaFullScreenViewController.h"
  #import "CameraViewController.h"
 #import "ImageLibraryViewController.h"
+ #import "PostToInstagramViewController.h"
 
 @interface ImagesTableViewController () <MediaTableViewCellDelegate, CameraViewControllerDelegate, ImageLibraryViewControllerDelegate>
 @property (nonatomic, strong) NSMutableArray *images;
@@ -86,13 +87,7 @@
 }
 
 - (void) cameraViewController:(CameraViewController *)cameraViewController didCompleteWithImage:(UIImage *)image {
-    [cameraViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+  [self handleImage:image withNavigationController:cameraViewController.navigationController];
 }
 - (void) dealloc
 {
@@ -389,13 +384,18 @@
 }
 
 - (void) imageLibraryViewController:(ImageLibraryViewController *)imageLibraryViewController didCompleteWithImage:(UIImage *)image {
-    [imageLibraryViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+    [self handleImage:image withNavigationController:imageLibraryViewController.navigationController];
+
+}
+
+- (void) handleImage:(UIImage *)image withNavigationController:(UINavigationController *)nav {
+    if (image) {
+        PostToInstagramViewController *postVC = [[PostToInstagramViewController alloc] initWithImage:image];
+        
+        [nav pushViewController:postVC animated:YES];
+    } else {
+        [nav dismissViewControllerAnimated:YES completion:nil];
+    }
 }
 
 
---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,14 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Michelle Rutherford on 8/16/15.
+//  Copyright (c) 2015 Michelle Rutherford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
